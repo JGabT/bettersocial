cc95ecc3d6d3b844d96d951731ad3318ce0814e5
jgabt <johngabtolentino@gmail.com>
2021-05-06 23:27:55 +0800

    add frontend 'wip'

---
+++ frontend/index.php
@@ -0,0 +1,23 @@
+<html>
+<link href='http://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="style.css">
+<div class="header">
+</div> 
+<div class="logo-header">  
+  <h2 class="logo-text"><b>b</b>etter<b>s</b>ocial</h2>
+</div>
+<div class="logo-header2">
+</div>
+<div class="login-background">
+  <form action="index.php">
+    <p class="email-text">email:</p>
+    <input class="username-box" type="text" name="name">
+    <p class="password-text">password:</p>
+    <input id="pswd" class="password-box" type="password" name="pswd">
+    <input class="login" type="submit" value="login">
+  </form>
+  <a href="test" class="forgot">forgot login?</a>
+</div>
+<div class="better-body">
+</div>
+</html>
