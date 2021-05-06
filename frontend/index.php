26b67bf0b91637fa1fd95a5ebe4f2dca661af213
jgabt <johngabtolentino@gmail.com>
2021-05-07 00:33:44 +0800

    update frontend html

--- frontend/index.php
+++ frontend/index.php
@@ -3,11 +3,13 @@
 <link rel="stylesheet" href="style.css">
 <div class="header">
 </div> 
+<!-- logo -->
 <div class="logo-header">  
   <h2 class="logo-text"><b>b</b>etter<b>s</b>ocial</h2>
 </div>
 <div class="logo-header2">
 </div>
+<!-- login-start -->
 <div class="login-background">
   <form action="index.php">
     <p class="email-text">email:</p>
@@ -20,4 +22,44 @@
 </div>
 <div class="better-body">
 </div>
+<!-- login-end -->
+<!-- signup-start -->
+<div class="better-signup">
+  <p class="semail-text">email:</p>
+  <input class="signup-usr-box" type="text" name="sname">
+  <p class="spass-text">password:</p>
+  <input class="signup-pass-box" type="password" name="sname">
+</div>
+<div class="better-signup-text">
+  <a class="better-sn-text">sign up for bettersocial</a> 
+</div>
+<!-- signup-end -->
+
+<!-- top part body start -->
+<div class="features">
+  <ul class="features-txt">
+    <li>view facebook and instagram videos in one feed</li>
+    <li>forward messages platform-to-platform </li>
+    <li>stay up to date with the latest social trends</li>
+    <li>view photos from different platforms in one platform</li>
+    <li>👉 poke you poke me poke everyone </li>
+  </ul>
+</div>
+<div class="notoads">
+  <span class="noad-text">no to free ads, yes to ads free.</span>
+</div>
+<div class="copyright">
+  <span class="copyright-txt"><a href="https://github.com/JGabT/">jgabt</a> | (c) bettersocial</span>
+</div>
+<!-- top part body stop -->
+
+<!-- bottom part body start -->
+<div class="better-botm">
+  <a class="better-botm-text"> bettersocial is a social utility to merge social platforms in one platform. </a>
+</div>
+
+<div class="better-logo-botm">
+  <a class="better-logo-text"><b>b</b>etter<b>s</b>ocial</a>
+</div>
+<!-- bottom part body end -->
 </html>
